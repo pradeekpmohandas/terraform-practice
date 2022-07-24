9080b952e70d3f8676eaadb7baf6914aee2a7b88
pradeekpmohandas <pradeek.pm@gmail.com>
2022-07-24 21:12:30 +0400

    main

--- 03-using-output/main.tf
+++ 03-using-output/main.tf
@@ -1,5 +1,5 @@
 resource "local_file" "foo" {
-    content  = "tesla model random_pet.carName.id"
+    content  = "tesla car pet ${random_pet.carName.id}"
     filename = "velichle/car.txt"
 }
 resource "random_pet" "carName" {
